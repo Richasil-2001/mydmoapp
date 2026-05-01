15e3e238ccd21384d93b7783843b49ff8f070e6e
Richa Sil <silricha2901@gmail.com>
2026-05-01 11:48:30 +0530

    feat:text changed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import CartCounter from './components/CartCounter';
 import './App.css';
 
+
 function App() {
   return (
     <div className="App">
--- src/components/CartCounter.js
+++ src/components/CartCounter.js
@@ -5,18 +5,20 @@ const CartCounter = () => {
     //state variable
     const[value,setValue] = useState(1);
     const incrementCounter=() =>{
-        if(value >= 20) return;
-        setValue(value+1);
+        if(value >= 20) return;        setValue(value+1);
     }
     const decrementCounter=() =>{
         if(value<1)return;
         setValue(value-1);
     }
+    const incrementButtonProps = value >= 20 ? {disabled:true} : {onClick:incrementCounter}
+        const decrementButtonProps = value <= 1 ? {disabled:true} : {onClick:decrementCounter}
+
     return(
         <>
-        {value>=20? <button disabled>+</button> : <button onClick={incrementCounter}>+</button>}
+        <button {...incrementButtonProps}>+</button>
         <h3>{value}</h3>
-        {value<=1?<button disabled>-</button> :<button onClick={decrementCounter}>-</button>}
+        <button {...decrementButtonProps}>-</button>
         </>
     )
 };
